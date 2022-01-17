f7c957b9527a1f320956ad9fee389292c04e2849
denerson28 <denerson2808@hotmail.com>
2022-01-17 16:37:57 -0300

    commit inicial

---
+++ cliente.py
@@ -0,0 +1,20 @@
+import socket
+
+def raw_input():
+    msg = str(input("digite uma mensagem: "))
+
+    return msg
+
+
+HOST = '127.0.0.1'     # Endereco IP do Servidor
+PORT = 5000            # Porta que o Servidor esta
+tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+dest = (HOST, PORT)
+tcp.connect(dest)
+print ('Para sair use CTRL+X\n')
+msg = raw_input()
+
+while msg != '\x18':
+    tcp.send(msg.encode())
+    msg = raw_input()
+tcp.close()
