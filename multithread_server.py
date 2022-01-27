648fb1d8f937028abd8b14490edcdaafd0e1d602
mairon.azevedo <mairon.azevedo@primeup.com.br>
2022-01-26 23:00:33 -0300

    deno

--- multithread_server.py
+++ multithread_server.py
@@ -2,6 +2,16 @@ import socket
 import os
 from _thread import *
 
+def multi_threaded_client(connection):
+    connection.send(str.encode('Server is working:'))
+    while True:
+        data = connection.recv(2048)
+        response = 'Server message: ' + data.decode('utf-8')
+        if not data:
+            break
+        connection.sendall(str.encode(response))
+    connection.close()
+
 ServerSideSocket = socket.socket()
 host = '127.0.0.1'
 port = 2004
@@ -14,15 +24,6 @@ except socket.error as e:
 print('Socket is listening..')
 ServerSideSocket.listen(5)
 
-def multi_threaded_client(connection):
-    connection.send(str.encode('Server is working:'))
-    while True:
-        data = connection.recv(2048)
-        response = 'Server message: ' + data.decode('utf-8')
-        if not data:
-            break
-        connection.sendall(str.encode(response))
-    connection.close()
 
 while True:
     Client, address = ServerSideSocket.accept()
@@ -30,4 +31,3 @@ while True:
     start_new_thread(multi_threaded_client, (Client, ))
     ThreadCount += 1
     print('Thread Number: ' + str(ThreadCount))
-ServerSideSocket.close()
